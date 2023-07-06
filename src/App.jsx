08391ccdc935970d1339cab0ea485ee1529eb955
ManuelNegrin <120143998+ManuelNegrin@users.noreply.github.com>
2023-07-06 15:53:20 -0300

    contenido agregado

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
+import Contacto from "./Components/Contacto/Contacto"
 import Home from "./Components/Home/Home"
 import Menu from "./Components/Menu/Menu"
+import Nosotros from "./Components/Nosotros/Nosotros"
 import Tienda from "./Components/Tienda/Tienda"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 
@@ -12,6 +14,8 @@ const App = () => {
       <Routes>
         <Route path="/" element ={<Home/>} />
         <Route path='/Tienda' element ={<Tienda/>} />
+        <Route path="/Nosotros" element ={<Nosotros/>}/>
+        <Route path="/Contacto" element ={<Contacto/>}/>
       </Routes>
     </BrowserRouter>
     </>
--- src/Components/Contacto/Contacto.jsx
+++ src/Components/Contacto/Contacto.jsx
@@ -3,7 +3,27 @@ import React from "react"
 const Contacto = (props) => {
   return (
     <div>
-      Contactenos con sus consultas
+      <section>
+            <p>Por lo general siempre surgen consultas de nuestra forma de trabajar, de nuestros diseños, o de nuestros plazos.</p>
+            <p>Aquí vas a poder evacuar todas estás dudas y más.</p>
+      </section>
+        <section>
+            <p>Dejanos tus datos junto con tu consulta para poder contactarnos contigo.</p>
+        </section>
+
+      <section>
+        <form>
+          <label>
+            Nombre
+            <input type="text" name="nombre"/>
+          </label>
+          <label>
+            Consulta
+            <input type="text" name="consulta"/>
+          </label>
+          <input type="submit" value="Enviar"/>
+        </form>
+      </section>
     </div>
   )
 };
